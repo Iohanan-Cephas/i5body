449160bd32b8668052a0efd3dcaecde658d60862
Joao Pedro Oliveira Barbosa <95698131+Iohanan-Cephas@users.noreply.github.com>
2026-08-19 11:21:16 -0300

    feat: setup project

---
+++ src/test/java/com/i5body/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i5body.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
